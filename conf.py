0932f9dbc9468ce7710c1b61f42c546ca4adcd3f
THESOVIE <105683270+THESOVIE@users.noreply.github.com>
2022-05-16 22:54:39 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 api_hash = "80ae053901c049b519ea5bf0f50e1675"
 api_id = 17345593
 bot_token = "5398790914:AAFdE8Lxz_cy9mF82_skghu1QkTM6EH4LEY"
-authorized_users = ['STANISLASKI']
+authorized_users = ['rockstar984']
 admin_user = 'STANISLASKI'
