b13e11d77c07718577ea395af36189a8aa040597
THESOVIE <105683270+THESOVIE@users.noreply.github.com>
2022-05-16 20:57:44 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
-api_hash = "apihash"
-api_id = 1736372819
-bot_token = "bottoken"
-authorized_users = []
-admin_user = 'admindelbot'
+api_hash = "80ae053901c049b519ea5bf0f50e1675"
+api_id = 17345593
+bot_token = "5398790914:AAFdE8Lxz_cy9mF82_skghu1QkTM6EH4LEY"
+authorized_users = ['STANISLASKI']
+admin_user = 'STANISLASKI'
